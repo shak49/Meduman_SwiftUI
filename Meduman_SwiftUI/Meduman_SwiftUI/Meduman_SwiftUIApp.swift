e4d2cceb30d4cef8e9331cb04335727528f26327
Shak <shakfeizi@gmail.com>
2021-12-21 13:47:50 -0600

    Project Initiated

---
+++ Meduman_SwiftUI/Meduman_SwiftUI/Meduman_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meduman_SwiftUIApp.swift
+//  Meduman_SwiftUI
+//
+//  Created by Shak Feizi on 12/21/21.
+//
+
+import SwiftUI
+
+@main
+struct Meduman_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
