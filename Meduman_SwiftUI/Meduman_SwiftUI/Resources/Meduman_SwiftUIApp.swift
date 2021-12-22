66864cf8b5adba72d10d3478c6b4ff78e74b44b5
Shak <shakfeizi@gmail.com>
2021-12-22 16:00:27 -0600

    Some Updates

--- Meduman_SwiftUI/Meduman_SwiftUI/Resources/Meduman_SwiftUIApp.swift
+++ Meduman_SwiftUI/Meduman_SwiftUI/Resources/Meduman_SwiftUIApp.swift
@@ -12,6 +12,14 @@ import Firebase
 struct Meduman_SwiftUIApp: App {
     init() {
         FirebaseApp.configure()
+        if ProcessInfo.processInfo.environment["unit_tests"] == "true" {
+          print("Setting up Firebase emulator localhost:8080")
+          let settings = Firestore.firestore().settings
+          settings.host = "localhost:8080"
+          settings.isPersistenceEnabled = false
+          settings.isSSLEnabled = false
+          Firestore.firestore().settings = settings
+        }
     }
     
     var body: some Scene {
