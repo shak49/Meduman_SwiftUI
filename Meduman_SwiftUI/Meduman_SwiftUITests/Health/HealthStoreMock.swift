a330c2eb4994ea756bb10b90fd54657d65c508ae
Shak49 <shakfeizi@gmail.com>
2022-10-03 22:03:58 -0500

    health repo test cases update

---
+++ Meduman_SwiftUI/Meduman_SwiftUITests/Health/HealthStoreMock.swift
@@ -0,0 +1,19 @@
+//
+//  HealthStoreMock.swift
+//  Meduman_SwiftUITests
+//
+//  Created by Shak Feizi on 10/3/22.
+//
+
+import HealthKit
+import Combine
+@testable import Meduman_SwiftUI
+
+
+protocol HealthStoreMockProtocol {
+    
+}
+
+class HealthStoreMock: HealthStoreMockProtocol {
+    
+}
