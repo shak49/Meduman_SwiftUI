9a8f9baf3f7c6303aa63c86331be90db6fb6a2bb
Shak49 <shakfeizi@gmail.com>
2022-10-12 20:29:39 -0500

    mockingbird removed / health repo mock refactored

---
+++ Meduman_SwiftUI/Meduman_SwiftUITests/Data/Health/HKHealthQueryMock.swift
@@ -0,0 +1,17 @@
+//
+//  HKHealthQueryMock.swift
+//  Meduman_SwiftUITests
+//
+//  Created by Shak Feizi on 10/12/22.
+//
+
+import HealthKit
+
+
+protocol HKHealthQueryProtocol {
+    
+}
+
+class HKHealthQueryMock: HKStatisticsCollectionQuery, HKHealthQueryProtocol {
+    
+}
