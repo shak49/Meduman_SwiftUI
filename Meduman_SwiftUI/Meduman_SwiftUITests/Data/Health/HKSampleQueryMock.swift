2d4b3f301aa31e17e627bc30210a85335a85cdce
shak49 <74379616+shak49@users.noreply.github.com>
2022-11-18 11:47:53 -0600

    Revert "Dev"

---
+++ Meduman_SwiftUI/Meduman_SwiftUITests/Data/Health/HKSampleQueryMock.swift
@@ -0,0 +1,26 @@
+//
+//  HKHealthQueryMock.swift
+//  Meduman_SwiftUITests
+//
+//  Created by Shak Feizi on 10/12/22.
+//
+
+//import HealthKit
+//
+//
+//class HKSampleQueryMock: HKSampleQuery {
+//    //MARK: - Properties
+//    let sampleQuery: HKSampleQuery?
+//    let sample: [HKSample]?
+//    let error: Error?
+//    
+//    //MARK: - Lifecycles
+//    override init(sampleType: HKSampleType, predicate: NSPredicate?, limit: Int, sortDescriptors: [NSSortDescriptor]?, resultsHandler: @escaping (HKSampleQuery, [HKSample]?, Error?) -> Void) {
+//        let query = self.sampleQuery
+//        let sample = self.sample
+//        let error = self.error
+//        super.init(sampleType: sampleType, predicate: predicate, limit: limit, sortDescriptors: sortDescriptors) { sampleQuery, samples, error in
+//            resultsHandler(query, sample, error)
+//        }
+//    }
+//}
