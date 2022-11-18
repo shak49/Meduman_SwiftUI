2e31a5e71a4821baef1cbe6b4246d3d90d80fb92
shak49 <74379616+shak49@users.noreply.github.com>
2022-11-18 12:11:27 -0600

    update

--- Meduman_SwiftUI/Meduman_SwiftUI/Data/Health/HealthRepository.swift
+++ Meduman_SwiftUI/Meduman_SwiftUI/Data/Health/HealthRepository.swift
@@ -12,26 +12,24 @@ import Combine
 protocol HealthRepoProtocol {
     //MARK: - Properties
     var healthStore: HKHealthStore? { get }
-    var healthQuary: HKSampleQuery? { get }
-    var healthTypes: Set<HKObjectType> { get }
+//    var healthQuary: HKSampleQuery? { get }
+//    var healthTypes: Set<HKObjectType> { get }
     
     //MARK: - Functions
-    func requestAuthorization() -> AnyPublisher<Bool, HealthError>
-    func writeCharacteristicTypeSample()
-    func readCharacteristicTypeSample()
-    func writeQuantityTypeSample()
-    func readQuantityTypeSample()
-    func writeCategoryTypeSample()
-    func readCategoryTypeSample()
+//    func requestAuthorization() -> AnyPublisher<Bool, HealthError>
+//    func writeCharacteristicTypeSample()
+//    func readCharacteristicTypeSample()
+//    func writeQuantityTypeSample()
+//    func readQuantityTypeSample()
+//    func writeCategoryTypeSample()
+//    func readCategoryTypeSample()
 }
 
 
 class HealthRepository: HealthRepoProtocol {
+    //MARK: - Properties
     var healthStore: HKHealthStore?
-    var healthQuary: HKSampleQuery?
     
-    init(healthStore: HKHealthStore?, healthQuary: HKSampleQuery?) {
-        self.healthStore = healthStore
-        self.healthQuary = healthQuary
-    }
+    //MARK: - Functions
+    
 }
