978c6a7cff7796bdba16933d3b85b7ac00a0ac46
shak49 <74379616+shak49@users.noreply.github.com>
2022-12-07 19:22:09 -0600

    update

--- Meduman_SwiftUI/Meduman_SwiftUITests/HealthRepositoryTests.swift
+++ Meduman_SwiftUI/Meduman_SwiftUITests/HealthRepositoryTests.swift
@@ -6,21 +6,23 @@
 //
 
 import XCTest
+import HealthKit
 @testable import Meduman_SwiftUI
 
 
 class HealthRepositoryTests: XCTestCase {
+    //MARK: - Properties
 
+    //MARK: - Lifecycles
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testRequestAuthorizationReturnTrue() {
+        try super.setUpWithError()
         
     }
 
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+        
+    }
+
+    //MARK: - Functions
 }
