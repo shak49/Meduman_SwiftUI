71756c96120e34725a8c3c90df4f42740fff1a68
shak49 <74379616+shak49@users.noreply.github.com>
2022-12-10 10:22:45 -0600

    update requestAuthorization test

--- Meduman_SwiftUI/Meduman_SwiftUI/Data/Health/HealthRepository.swift
+++ Meduman_SwiftUI/Meduman_SwiftUI/Data/Health/HealthRepository.swift
@@ -20,10 +20,10 @@ protocol HealthRepoProtocol {
     
     //MARK: - Functions
     func requestAuthorization() -> Future<Bool, HKError>
+    func writeQuantityTypeSample(record: Double?) -> AnyPublisher<Bool, HKError>
+    //func readQuantityTypeSample()
 //    func writeCharacteristicTypeSample()
 //    func readCharacteristicTypeSample()
-//    func writeQuantityTypeSample()
-//    func readQuantityTypeSample()
 //    func writeCategoryTypeSample()
 //    func readCategoryTypeSample()
 }
@@ -56,9 +56,31 @@ class HealthRepository: HealthRepoProtocol {
                     promise(.failure(.unableToAuthorizeAccess))
                     return
                 }
-                print(success)
+                print("SUCCESS: \(success)")
                 promise(.success(success))
             })
         }
+    }
+    
+    func writeQuantityTypeSample(record: Double?) -> AnyPublisher<Bool, HKError> {
+        let subject = PassthroughSubject<Bool, HKError>()
+        if let record = record {
+            guard let bloodGlucose = HKQuantityType.quantityType(forIdentifier: .bloodGlucose) else {
+                fatalError("Step Count Type is no longer available in HealthKit")
+            }
+            let unit = HKUnit(from: "mg/dL")
+            let quantity = HKQuantity(unit: unit, doubleValue: record)
+            let sample = HKQuantitySample(type: bloodGlucose, quantity: quantity, start: Date(), end: Date())
+            self.healthStore?.save(sample, withCompletion: { success, error in
+                guard error == nil else {
+                    subject.send(completion: .failure(.unableToWriteHealthRecord))
+                    return
+                }
+                if success != nil {
+                    subject.send(true)
+                }
+            })
+        }
+        return subject.eraseToAnyPublisher()
     }
 }
