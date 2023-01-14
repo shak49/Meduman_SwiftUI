88a47246f966f89f16659597804f06b177f73f55
shak49 <74379616+shak49@users.noreply.github.com>
2023-01-14 12:13:51 -0600

    update

---
+++ Meduman_SwiftUI/Meduman_SwiftUI/Extensions/Units.swift
@@ -0,0 +1,14 @@
+//
+//  Identifiers&Units.swift
+//  Meduman_SwiftUI
+//
+//  Created by Shak Feizi on 1/14/23.
+//
+
+import HealthKit
+
+
+enum HealthUnit: String {
+    case glucoseUnit = "mg/dL"
+    case heartRateUnit
+}
