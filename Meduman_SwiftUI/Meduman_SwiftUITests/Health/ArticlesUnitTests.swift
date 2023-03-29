dd9d70aeb0e1ceeb641a9b1280039e4edaf83f74
shak49 <74379616+shak49@users.noreply.github.com>
2023-03-28 23:36:26 -0500

    articles unit test initiated / memory leaks checked

---
+++ Meduman_SwiftUI/Meduman_SwiftUITests/Health/ArticlesUnitTests.swift
@@ -0,0 +1,28 @@
+//
+//  ArticlesUnitTests.swift
+//  Meduman_SwiftUITests
+//
+//  Created by Shak Feizi on 3/28/23.
+//
+
+import XCTest
+import Cuckoo
+
+
+class ArticlesUnitTests: XCTestCase {
+    //MARK: - Properties
+    
+    //MARK: - Lifecycles
+    override func setUpWithError() throws {
+        
+    }
+
+    override func tearDownWithError() throws {
+        
+    }
+
+    //MARK: - Functions
+    func test_articlesUrl_validUrl() {
+        
+    }
+}
