e1b258e729e6ec43571d6b1111144219c3433578
shak49 <74379616+shak49@users.noreply.github.com>
2023-04-05 22:45:04 -0500

    article repo mock removed / cuckoo added

--- Meduman_SwiftUI/Meduman_SwiftUI/Data/Health/ArticleRepository.swift
+++ Meduman_SwiftUI/Meduman_SwiftUI/Data/Health/ArticleRepository.swift
@@ -12,33 +12,28 @@ import Alamofire
 
 protocol ArticleRepositoryProtocol {
     //MARK: - Properties
-    var session: Session? { get }
     
     //MARK: - Lifecycles
-    init(session: Session?)
     
     //MARK: - Functions
-    func fetchArticles() async -> AnyPublisher<[Article]?, NetworkError>
+    //func fetchArticles(queryItems: [URLQueryItem]?) async -> AnyPublisher<[Article]?, NetworkError>?
 }
 
 
 class ArticleRepository: ArticleRepositoryProtocol {
     //MARK: - Properties
-    var session: Session?
     
     //MARK: - Lifecycles
-    required init(session: Session?) {
-        self.session = session
-    }
     
     //MARK: - Functions
-    func fetchArticles() async -> AnyPublisher<[Article]?, NetworkError> {
-        let queryItems = [
-            URLQueryItem(name: "Type", value: "topic"),
-            URLQueryItem(name: "Lang", value: "en"),
-            URLQueryItem(name: "Lang", value: "en")
-        ]
-        guard let url = Constructor.shared.url(scheme: "https", host: "health.gov", path: "/v3/itemlist.json", queryItems: queryItems, token: nil, headerField: nil) else { throw NetworkError.badUrl }
-        
-    }
+//    func fetchArticles(queryItems: [URLQueryItem]?) async -> AnyPublisher<[Article]?, NetworkError>? {
+//        let subject = PassthroughSubject<[Article]?, NetworkError>()
+//        guard let queryItems = queryItems else {
+//            return nil }
+//        guard let url = Constructor.shared.url(scheme: "https", host: "health.gov", path: "/v3/itemlist.json", queryItems: queryItems, token: nil, headerField: nil) else {
+//            subject.send(completion: .failure(.badUrl))
+//            return nil
+//        }
+//
+//    }
 }
