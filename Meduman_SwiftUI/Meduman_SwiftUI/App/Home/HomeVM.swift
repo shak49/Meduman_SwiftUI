dfa0040353aef578fb0e210321fe3d8761b92578
shak49 <74379616+shak49@users.noreply.github.com>
2024-02-12 20:52:14 -0600

    wip

--- Meduman_SwiftUI/Meduman_SwiftUI/App/Home/HomeVM.swift
+++ Meduman_SwiftUI/Meduman_SwiftUI/App/Home/HomeVM.swift
@@ -7,14 +7,80 @@
 
 import Foundation
 import HealthKit
+import Combine
 
+
+struct RecordVM: Hashable, Identifiable {
+    var quantitySample: HKQuantitySample?
+    var id: String = UUID().uuidString
+    var quantity: String {
+        guard let quantitySample = quantitySample else { return "" }
+        return "\(quantitySample.quantity)"
+    }
+    var type: HKQuantityType? {
+        guard let quantitySample = quantitySample else { return nil }
+        return quantitySample.quantityType
+    }
+    var sample: HKSampleType? {
+        guard let quantitySample = quantitySample else { return nil  }
+        return quantitySample.sampleType
+    }
+    var endDate: String {
+        guard let quantitySample = quantitySample else { return "" }
+        return "\(quantitySample.endDate)"
+    }
+}
 
 class HomeVM: ObservableObject {
     //MARK: - Prooperties
-    private var service = HealthService(healthStore: HKHealthStore())
+    private var service: HealthService? = HealthService(healthStore: HKHealthStore())
+    private var cancellables = Set<AnyCancellable>()
+    private let healthSamples = [
+        HKSampleType.quantityType(forIdentifier: .bloodGlucose),
+        HKSampleType.quantityType(forIdentifier: .heartRate),
+        HKSampleType.quantityType(forIdentifier: .bloodPressureSystolic)
+    ]
+    @Published var records: [String : [RecordVM]] = [
+        "Glucose" : [],
+        "Pressure" : [],
+        "Rate" : []
+    ]
     
     //MARK: - Lifecycles
+    init() {
+        getSamples()
+    }
     
     //MARK: - Functions
-
+    func getSamples() {
+        for sample in healthSamples {
+            readRecord(type: sample)
+        }
+    }
+    
+    func readRecord(type: HKSampleType?) {
+        guard let type = type else { return }
+        self.service?.readHealthRecord(type: type)
+            .receive(on: DispatchQueue.main)
+            .sink(receiveCompletion: { completion in
+                switch  completion {
+                case .finished:
+                    print(completion)
+                case .failure(let error):
+                    print("> ERROR:", error.localizedDescription)
+                }
+            }, receiveValue: { samples in
+                guard let samples = samples else { return }
+                samples.forEach { sample in
+                    if sample.quantityType == .init(.bloodGlucose) {
+                        self.records["Glucose"]?.append(RecordVM(quantitySample: sample))
+                    } else if sample.quantityType == .init(.bloodPressureSystolic) {
+                        self.records["Pressure"]?.append(RecordVM(quantitySample: sample))
+                    } else {
+                        self.records["Rate"]?.append(RecordVM(quantitySample: sample))
+                    }
+                }
+            })
+            .store(in: &cancellables)
+    }
 }
--- Meduman_SwiftUI/Meduman_SwiftUI/App/Home/HomeView.swift
+++ Meduman_SwiftUI/Meduman_SwiftUI/App/Home/HomeView.swift
@@ -14,15 +14,32 @@ struct HomeView: View {
     
     //MARK: - Body
     var body: some View {
-        ScrollView {
+        VStack {
             Chart {
-                
+                ForEach(vm.records.sorted { $0.key < $1.key }, id: \.key) { key, value in
+                    ForEach(value) { sample in
+                        LineMark(x: .value("date", sample.endDate), y: .value("record", sample.quantity))
+                            .foregroundStyle(by: .value("type", key))
+                    }
+                }
             }
-            .background(.gray)
+            .chartXAxis {
+                AxisMarks(values: .stride(by: .year)) { _ in
+                    AxisGridLine()
+                    AxisValueLabel(format: .dateTime.year(.defaultDigits))
+                }
+            }
+                .frame(height: 200)
+            Rectangle()
+                .frame(height: 200)
+                .foregroundColor(.gray)
+            Spacer()
         }
+        .padding(.horizontal, 16)
     }
 }
 
 #Preview {
     HomeView()
 }
+
