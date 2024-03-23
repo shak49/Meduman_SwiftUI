86eea5fc4fd1e4bd303acc0876920da911d48383
Shak Feizi <>
2024-03-22 23:46:17 -0500

    ArticleView implemented / some refactoring made

---
+++ Meduman_SwiftUI/Meduman_SwiftUI/App/Home/ArticleView.swift
@@ -0,0 +1,41 @@
+//
+//  ArticleView.swift
+//  Meduman_SwiftUI
+//
+//  Created by Shak Feizi on 3/22/24.
+//
+
+import SwiftUI
+
+struct ArticleView: View {
+    //MARK: - Properties
+    let article: Article
+    
+    //MARK: - Body
+    var body: some View {
+        ScrollView {
+            VStack(spacing: 16) {
+                AsyncImage(url: article.image) { image in
+                    image
+                        .resizable()
+                        .scaledToFit()
+                } placeholder: {}
+                VStack(alignment: .leading, spacing: 16) {
+                    Text(article.title ?? "")
+                        .font(.system(size: 26))
+                        .bold()
+                    ForEach(article.section?.paragraphs ?? []) { paragraph in
+                        Text(paragraph.body ?? "")
+                    }
+                }
+                .padding(.horizontal, 16)
+                .padding(.top, 32)
+            }
+        }
+        .ignoresSafeArea()
+    }
+}
+
+#Preview {
+    ArticleView(article: Article(id: "", title: nil, imageString: nil, section: Section(paragraphs: [])))
+}
