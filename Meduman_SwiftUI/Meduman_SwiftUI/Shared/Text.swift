dea4475423b507eb7e31f9b5efd0b1f6e0650e45
Shak Feizi <>
2024-03-23 22:38:25 -0500

    wip

---
+++ Meduman_SwiftUI/Meduman_SwiftUI/Shared/Text.swift
@@ -0,0 +1,33 @@
+//
+//  LocalString.swift
+//  Meduman_SwiftUI
+//
+//  Created by Shak Feizi on 3/15/24.
+//
+
+import Foundation
+
+
+enum UIText {
+    //MARK: - Navigation Tab Titles
+    static let article = "Article"
+    static let health = "Health"
+    static let home = "Home"
+    static let reminder = "Reminder"
+    static let successAlertMessage = "Successfully provided with health recommendations!"
+    //MARK: - Dummy & Empty
+    static let empty = ""
+    static let welcomeTo = "Welcome To "
+    //MARK: - Buttons
+    static let submit = "Submit"
+    //MARK: - Lables
+    static let alertTitle = "Enter your info"
+    static let dailyProgress = "Daily Progress"
+    //MARK: - LineMark Keys
+    static let date = "date"
+    static let record = "record"
+    static let type = "type"
+    //MARK: - Placeholder Text
+    static let age = "Age"
+    static let sex = "Sex"
+}
